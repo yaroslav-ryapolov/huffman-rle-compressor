4d15c62ca853f779b2f0117092687541f20aab1d
ZedRoth <zedroth@phone-book.(none)>
2012-12-22 18:48:04 +0700

    [Hamming] Some thoughts

---
+++ src/com/harrycodeman/compression/IBitGroup.java
@@ -0,0 +1,12 @@
+package com.harrycodeman.compression;
+
+public interface IBitGroup {
+    // TODO: building from InputString
+    // TODO: size up to 32
+    // TODO: building from Integer
+    // TODO: converting to Integer
+
+
+    public boolean readBit();
+    public int getAsInteger();
+}
---
+++ src/com/harrycodeman/compression/ISimpleInput.java
@@ -0,0 +1,17 @@
+package com.harrycodeman.compression;
+
+public interface ISimpleInput {
+    // TODO: read one bit
+    // TODO: read n bits
+    // TODO: read one byte
+    // TODO: bufferized reading
+
+    // TODO: write one bit
+    // TODO: write n bits
+    // TODO: write one byte
+    // TODO: write n bytes
+    // TODO: bufferized writing
+
+    public byte readByte();
+    public boolean readBit();
+}
